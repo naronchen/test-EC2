4f4e6161b839233f325ea15c9fcb220c2019ca81
Naron <naron.chen@richmond.edu>
2024-04-03 23:57:53 -0400

    lambda ready

---
+++ lambda/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+// AWS SES part
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+
+const ses = new AWS.SES();
+
+exports.handler = async (event) => {
+    const currentTime = Math.floor(Date.now() / 1000);
+    let { data, error } = await supabase
+        .from('emails')
+        .select('letter, email')
+        .lte('time_to_send', currentTime);
+    if (error) {
+        console.error('Error fetching data:', error);
+        return { statusCode: 500, body: 'Failed to fetch data' };
+    }
+
+    // process and send email
+    for (const row of data) {
+        const {email, letter} = row;
+        const params = {
+            Source: 'naron.chen1@gmail.com',
+            Destination: {
+                ToAddresses: [email]
+            },
+            Message:{
+                Subject:{
+                    Data: 'Time-Capsule Email', // @TODO: should allow user to set subject
+                },
+                Body:{
+                    Text:{
+                        Data: letter
+                    }
+                }
+            }
+        }
+        try {
+            await ses.sendEmail(params).promise();
+            console.log(`Email sent to ${email}`);
+        } catch (sendError) {
+            console.error(`Error sending email to ${email}:`, sendError);
+        }
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+};
+
+(async () => {
+    const response = await exports.handler();
+    console.log(response.body);
+})();
